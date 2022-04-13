c2a6d56911fc7dee1b8687ef057d57f91318bee7
Miriam debian <m.g.cubillo@gmail.com>
2022-04-13 15:32:25 +0100

    parse objects

--- parse/parse.c
+++ parse/parse.c
@@ -6,41 +6,42 @@
 /*   By: migarcia <migarcia@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 18:12:04 by migarcia          #+#    #+#             */
-/*   Updated: 2022/04/09 13:04:08 by migarcia         ###   ########.fr       */
+/*   Updated: 2022/04/06 20:06:13 by migarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "../libft/get_next_line.h"
-#include "../algft/inc/algft.h"
+#include "../../includes/algft.h"
 #include "./parse.h"
 #include <fcntl.h>
 
-int	ft_parsing_type(t_scene *scene, char *str)
+int	ft_parsing_type(t_scene *scene, t_camera *c, t_world *world, char *str)
 {
 	(void) str;
 	int	check;
 
-	if (str[0] == 'A' && ft_is_spacetab(str[1]))
+	check = 0;
+	if (str[0] == 'A' && ft_isspacetab(str[1]))
 		check = ft_parse_ambient_light(scene, str);
-	else if (str[0] == 'C' && ft_is_spacetab(str[1]))
-		check = ft_parse_camera(scene, str);
-/*	else if (str[0] == 'L' && ft_is_spacetab(str[1]))
-		check = ft_parse_light(world, str);
-	else if (str[0] == 's' && str[1] == 'p' && ft_is_spacetab(str[2]))
-		check = ft_parse_sphere(world, str);
-	else if (str[0] == 'p' && str[1] == 'l' && ft_is_spacetab(str[2]))
-		check = ft_parse_plane(world, str);
-	else if (str[0] == 'c' && str[1] == 'y' && ft_is_spacetab(str[2]))
-		check = ft_parse_cylinder(world, str);
+	else if (str[0] == 'C' && ft_isspacetab(str[1]))
+		check = ft_parse_camera(scene, c, str);
+	else if (str[0] == 'L' && ft_isspacetab(str[1]))
+		check = ft_parse_light(scene, world, str);
+	else if (str[0] == 's' && str[1] == 'p' && ft_isspacetab(str[2]))
+		check = ft_parse_sphere(scene, str);
+	else if (str[0] == 'p' && str[1] == 'l' && ft_isspacetab(str[2]))
+		check = ft_parse_plane(scene, str);
+	else if (str[0] == 'c' && str[1] == 'y' && ft_isspacetab(str[2]))
+		check = ft_parse_cylinder(scene, str);
 	else if (str[0] == '\n')
 		return (0);
 	else
-		check = return (ft_error("character invalid.\n", 2));*/
-	return (0);
+		return(ft_error("character invalid."));
+	return (check);
 }
 
-int	ft_parse(t_scene *scene, char *file)
+int	ft_parse(t_scene *scene, t_camera *c, t_world *world, char *file)
 {
 	int		fd;
 	char	*line;
@@ -55,14 +56,16 @@ int	ft_parse(t_scene *scene, char *file)
 		check = get_next_line(fd, &line);
 		if (!check)
 			break ;
-		check = ft_parsing_type(scene, line);
+		check = ft_parsing_type(scene, c, world, line);
 		free(line);
 	}
+	if (!check)
+		free(line);
 	close (fd);
 	return (0);
 }
 
-int	ft_check_file(t_scene *scene, char *file)
+int	ft_check_file(t_scene *scene, t_camera *cam, t_world *world, char *file)
 {
 	int	fd;
 	int	len;
@@ -73,12 +76,12 @@ int	ft_check_file(t_scene *scene, char *file)
 	e_len = 2;
 	ext = ".rt";
 	len = ft_strlen(file) - 1;
-	while (ext[e_len])
+	/*while (ext[e_len])
 	{
 		if (len < 1 || ext[e_len] != file[len])
                 	ft_exit("Invalid extension file.", 1);
 		e_len--;
 		len--;
-	}
-	return (ft_parse(scene, file));
+	}*/
+	return (ft_parse(scene, cam, world, file));
 }
